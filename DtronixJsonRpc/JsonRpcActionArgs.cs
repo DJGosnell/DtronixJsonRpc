9a3b48ea6b203527754f311060a6e6ea0c08038b
DJGosnell <DJGosnell@gmail.com>
2015-10-11 23:02:04 -0400

    Added INotifyPropertyChanged to be part of the default ActionArgs.

--- DtronixJsonRpc/JsonRpcActionArgs.cs
+++ DtronixJsonRpc/JsonRpcActionArgs.cs
@@ -2,13 +2,17 @@
 using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DtronixJsonRpc {
-    public class JsonRpcActionArgs {
+    public class JsonRpcActionArgs : INotifyPropertyChanged {
 		public JsonRpcSource Source { get; set; } = JsonRpcSource.Unset;
+
+		public event PropertyChangedEventHandler PropertyChanged;
 
 		protected void RequireStringLength(string str, int min, int max, bool allow_null, [System.Runtime.CompilerServices.CallerMemberName] string member_name = "") {
 			if (str.Length < min) {
@@ -18,6 +22,15 @@ namespace DtronixJsonRpc {
 			} else if (allow_null == false && str == null) {
 				throw new InvalidOperationException($"{member_name} must not be null.");
 			}
+		}
+
+		protected bool SetField<T>(ref T field, T value, [CallerMemberName] string property_name = null) {
+			if (EqualityComparer<T>.Default.Equals(field, value)) {
+				return false;
+			}
+			field = value;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name));
+			return true;
 		}
 	}
 }
